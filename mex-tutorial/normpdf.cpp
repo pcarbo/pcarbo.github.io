f4ba0cb99512047fa83be8a0d708977e74b974ea
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-01-18 14:45:43 -0600

    Added several other random files.

---
+++ mex-tutorial/normpdf.cpp
@@ -0,0 +1,54 @@
+#include "mex.h"
+#include <math.h>
+
+using namespace std;
+
+#define pi (3.141592653589793)
+
+extern void _main();
+
+const int numInputArgs  = 3;
+const int numOutputArgs = 1;
+
+// Function declarations.
+// -----------------------------------------------------------------
+double  getMatlabScalar    (const mxArray* ptr);
+double& createMatlabScalar (mxArray*& ptr);
+
+// Function definitions.
+// -----------------------------------------------------------------
+void mexFunction (int nlhs, mxArray *plhs[],
+		  int nrhs, const mxArray *prhs[]) {
+  
+  // Check to see if we have the correct number of input and output
+  // arguments.
+  if (nrhs != numInputArgs)
+    mexErrMsgTxt("Incorrect number of input arguments");
+  if (nlhs != numOutputArgs)
+    mexErrMsgTxt("Incorrect number of output arguments");
+
+  // Get the inputs.
+  double x  = getMatlabScalar(prhs[0]);
+  double mu = getMatlabScalar(prhs[1]);
+  double v  = getMatlabScalar(prhs[2]);
+
+  // Create the output. It is also a double-precision scalar.
+  double& p = createMatlabScalar(plhs[0]);
+
+  // Compute the value of the univariate Normal at x.
+  p = exp(-(x-mu)*(x-mu)/(2*v)) / sqrt(2*pi*v);
+}
+
+double getMatlabScalar (const mxArray* ptr) {
+
+  // Make sure the input argument is a scalar in double-precision.
+  if (!mxIsDouble(ptr) || mxGetNumberOfElements(ptr) != 1)
+    mexErrMsgTxt("The input argument must be a double-precision scalar");
+
+  return *mxGetPr(ptr);
+}
+
+double& createMatlabScalar (mxArray*& ptr) { 
+  ptr = mxCreateDoubleMatrix(1,1,mxREAL);
+  return *mxGetPr(ptr);
+}
